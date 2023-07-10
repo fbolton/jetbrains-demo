83016631a29cac22f0c93a62083f0ec0a1262c8d
Fintan Bolton <techwriter@pure-corba.com>
2023-07-10 17:10:42 +0200

    Added Space automation script

---
+++ .space.kts
@@ -0,0 +1,15 @@
+job("Build and publish") {
+    container(displayName = "Build and notify", image = "gradle:7.2-jre11") {
+        kotlinScript { api ->
+            val channel = ChannelIdentifier.Channel(ChatChannel.FromName("developers"))
+            try {
+                api.gradlew("build")
+                val content = ChatMessage.Text("Build succeeded")
+                api.space().chats.messages.sendMessage(channel = channel, content = content)
+            } catch (ex: Exception) {
+                val content = ChatMessage.Text("Build failed")
+                api.space().chats.messages.sendMessage(channel = channel, content = content)
+            }
+        }
+    }
+}
