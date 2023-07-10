45a684fbe8f03be4cca0cf7003dd191c9da75da8
Fintan Bolton <techwriter@pure-corba.com>
2023-07-10 17:43:52 +0200

    Add code to script for automatically raising an issue

--- .space.kts
+++ .space.kts
@@ -3,14 +3,32 @@ job("Build and publish") {
         kotlinScript { api ->
             val channel = ChannelIdentifier.Channel(ChatChannel.FromName("developers"))
             try {
-                api.gradlew("build")
+                api.gradlew("build --stacktrace")
                 val content = ChatMessage.Text("Build succeeded")
                 api.space().chats.messages.sendMessage(channel = channel, content = content)
             } catch (ex: Exception) {
                 val content = ChatMessage.Text("Build failed")
                 api.space().chats.messages.sendMessage(channel = channel, content = content)
-                val errMsg = ChatMessage.Text("${ex.message}")
-                api.space().chats.messages.sendMessage(channel = channel, content = content)
+
+
+                // get project Id
+                val id = api.projectId()
+                // get current build run number
+                val runNumber = api.executionNumber()
+
+                //get all issue statuses
+                val statuses = api.space().projects.planning.issues.statuses.
+                getAllIssueStatuses(project = ProjectIdentifier.Id(id))
+                //get id of 'Open' issue status
+                val openStatusId = statuses.find { it.name == "Open" }?.id
+                    ?: throw kotlin.Exception("The 'Open' state doesn't exist in the project")
+                // create issue with 'Open' status
+                api.space().projects.planning.issues.createIssue(
+                    project = ProjectIdentifier.Id(id),
+                    // generate name based on build run number
+                    title = "Job 'Build and publish' #$runNumber failed",
+                    description = "${ex.message}",
+                    status = openStatusId
             }
         }
     }
