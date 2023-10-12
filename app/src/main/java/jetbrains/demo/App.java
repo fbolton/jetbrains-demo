d4a214de1c6a89fb522861d4c88e5c725a4f4099
Fintan Bolton <fintan.bolton@jetbrains.com>
2023-10-12 16:24:48 +0200

    Yet another comment

--- app/src/main/java/jetbrains/demo/App.java
+++ app/src/main/java/jetbrains/demo/App.java
@@ -5,6 +5,7 @@ package jetbrains.demo;
 
 // Added a comment to this class
 // Add another comment to see updates in TC
+// Yet another comment for testing
 public class App {
     public String getGreeting() {
         return "Hello World!";
